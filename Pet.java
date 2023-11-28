f08dcecff2072b2704ebbdabe841bc3249cf066b
echatfield61 <echatfield61@gmail.com>
2023-11-28 13:17:00 -0500

    YAY!
    YAY!

--- Pet.java
+++ Pet.java
@@ -1,3 +1,51 @@
 public class Pet {
+    private String name;
+    private int age;
+    private double weight;
+    private String type;
+    private String breed;
+
+    public Pet()
+    {
+        name = "unknown";
+        age = 0;
+        weight = 0;
+        type = "unknown";
+        breed = "unknown";
+
+    }
+    public Pet(String n, int a, double w,String t,String b)
+    {
+        name = n;
+        age=a;
+        weight=w;
+        type=t;
+        breed=b;
+    }
+    
+    public String getName()
+    {
+        return name;
+    }
+
+    public int getAge()
+    {
+        return age;
+    }
+
+    public double getWeight()
+    {
+        return weight;
+    }
+
+    public String getType()
+    {
+        return type;
+    }
+
+    public String getBreed()
+    {
+        return breed;
+    }
     
 }
